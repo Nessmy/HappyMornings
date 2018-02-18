a947b7202b5cc786bbd4afd2b4f20ac9954e8e8f
Nessmy <sabri_smine94@hotmail.com>
2018-02-18 09:26:42 +0100

    Testing splash intro
    Testing splash intro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,13 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   splash = true;
-  constructor(public navCtrl: NavController) {
+  posts: any;
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get('https://www.reddit.com/r/mademesmile/top/.json?limit=50').map(res => res.json()).subscribe(data => {
+      this.posts = data.data.children;
+      });
+
 
   }
   ionViewDidLoad() {
